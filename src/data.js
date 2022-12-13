c97de8688676add7835520a068328bbba461e41a
Cony GuGa <constanza.garcia.gtz@gmail.com>
2022-12-13 17:22:02 -0600

    Pasos Lentos

--- src/data.js
+++ src/data.js
@@ -1,17 +1,46 @@
 // estas funciones son de ejemplo
+// export const sortDataTypeAsc = (dp) => {
+export const sortData = (dp, option) => {
+  if (option == "optNumAsc") {
+    let sortDp = dp.sort((a, b) => {
+      return ((a.type[0]).localeCompare(b.type[0]));
+      // return console.log(a.type, b.type);
+    });
+    //return
+  }
+  if (option == "numDesc") {
+    let sortDp = dp.sort((a, b) => {
+      return -((a.type[0]).localeCompare(b.type[0]));
+      // return console.log(a.type, b.type);
+    });
+    //return
+  }
+  // let valueOption = option.value;
+  // let sortType = dp.sort((a, b) => {
+    // return ((a.type[0]).localeCompare(b.type[0]));//¿Por qué los parentesis externos?
+    // return console.log(a.type, b.type);
+  // });
+  // console.log(dp)
+  return sortDp
+
+}
+// console.log(sortDataTypeAsc(dp));
+
+// const items = ["réservé", "premier", "communiqué", "café", "adieu", "éclair"];
+// items.sort((a, b) => a.localeCompare(b));
+
+// items is ['adieu', 'café', 'communiqué', 'éclair', 'premier', 'réservé']
 
 
 
 
+// export const example = () => {
+//   return 'example';
+// };
 
-
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
 
 
 // const sortData = (data, sortBy, sortOrder) => {
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import { example } from './data.js';
+import { sortData } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
@@ -10,71 +10,83 @@ let container = document.getElementById("cardsContainer")
 
 // Obtener parte de la data
 
-let loadData = data.pokemon.map(function (datum) {
-    return {
-        img: datum.img,
-        name: datum.name,
-        num: datum.num,
-        type: datum.type,
-        generation: datum.generation
-    }
-})
+let loadData = data.pokemon
 
 // Mostrar todos los Pokémons en tarjetas sólo con los datos que trabajamos
 
-showData(loadData);
+
 
 // Función que muestra sólo data con la que trabajamos
 
 function showData(loadData) {
-    loadData.map(function (data) {
+    container.innerHTML = "";
+    loadData.map(function (item) {
 
         // Crear tarjeta contenedora de datos para cada Pokémon
         let card = document.createElement("div");
 
         // Adjuntar/Incluir "card" dentro de "conteiner"
-        container.append(card);
+        // container.appendChild(card);
 
         // Añadir/incluir todos los item de la card Pokemon
         card.innerHTML = `
-<div id="container-pokeImage" class="pokeData">
-    <img src="${data.img}"/>
-</div>
-<div id="container-pokeNum_Name" class="pokeData">
-    <h3 id="pokeName">${data.name}</h3>
-    <h3 id="pokeNum">${data.num}</h3>
-</div>
-<div id="container-pokeType" class="pokeData">
-    <h5 id="pokeType">${data.type}</h5>
-</div>
-<div id="container-pokeGeneration" class="pokeData">
-    <h5 id="pokeGenerationName">${data.generation.name}</h5>
-    <h5 id="pokeGenerationNum">${data.generation.num}</h5>
-</div>
-`
+            <div id="container-pokeImage" class="pokeData">
+                <img src="${item.img}"/>
+            </div>
+            <div id="container-pokeNum_Name" class="pokeData">
+                <h3 id="pokeName">${item.name}</h3>
+                <h3 id="pokeNum">${item.num}</h3>
+            </div>
+            <div id="container-pokeType" class="pokeData">
+                <h5 id="pokeType">${item.type}</h5>
+            </div>
+            <div id="container-pokeGeneration" class="pokeData">
+                <h5 id="pokeGenerationName">${item.generation.name}</h5>
+                <h5 id="pokeGenerationNum">${item.generation.num}</h5>
+            </div>`
+        container.appendChild(card);
+        // return console.log("Ahora sí");
     })
+    // console.log(mapingData);
 }
+showData(loadData);
 
-// console.log(loadData)
+// console.log(mapingData.name);
 
 // ORDENAR POKEMONS POR TIPO Y NUMERO DE CREACION
 // Traer botones desde HTML y agregar escuchador de eventos
-let btnType = document.getElementById("btnType");
-btnType.addEventListener("click", () => { sortData(loadData) });
-let btnNum = document.getElementById("btnNum");
-btnNum.addEventListener("click", () => { sortData(loadData) });
+// let btnType = document.getElementById("btnType");
+// btnType.addEventListener("click", () => {
+//     let viweData = sortDataTypeAsc(loadData);
+//     // let viweData = sortDataTypeAsc(loadData, option);
+//     showData(viweData)
+// });
+// let opt1 = document.getElementById("optNumAsc").value;
+// let opt2 = document.getElementById("optNumDes").value;
+// let opt3 = document.getElementById("optTypAsc").value;
+// let opt4 = document.getElementById("optTypDes").value;
 
-function sortData(loadData) {
-    // console.log(loadData);
+// let opt1 = document.querySelector("#optNumAsc").value;
+// let opts = document.querySelectorAll("option#id");
+let selectOrder = document.getElementById("orderSelect");
+selectOrder.addEventListener("change", (e) => {
+    for (let i = 0; i < 4; i++) {
+        // const element = array[index];
+        console.log(e.target[i].value);
+    }
+    // let viweData = sortDataTypeAsc(loadData);
+    // let viweData = sortData(loadData, option);
+    // showData(viweData)
     // console.log(e);
-
-    loadData.sort((a, b) => {
-        if (a.type[0] < b.type[0])
-        return 1
+    // console.log(e.target);
+    // console.log(e.target.value);
 });
+// let btnNum = document.getElementById("btnNum");
+// btnNum.addEventListener("click", () => { sortData(loadData) });
 
-}
-console.log(loadData[0])
+
+// console.log(opt1, opt2, opt3, opt4);
+// console.log(loadData[0].type);
 // Para Aprender----------------------------------------------------------------------------------------------------------------------------------------
 
 // const typeColor = {
@@ -164,3 +176,10 @@ console.log(loadData[0])
 
 // // console.log(data.pokemon[0].generation.name);
 // // console.log(example, data);
+
+//Filtro De Sandy y Vicky
+//método de filtrar por productor
+// filtrarPorProductor(peliculas,nameProducer){
+//     let peliculasdelproductor= peliculas.filter(pelicula =>pelicula.producer===nameProducer);
+//     return peliculasdelproductor
+//  },
